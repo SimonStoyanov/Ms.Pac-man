ab91370cef4bd0e557ebcb2fb348f1dcb6bea4e1
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2016-03-31 18:51:14 +0200

    Testing the map

--- Ms. Pacman/Globals.h
+++ Ms. Pacman/Globals.h
@@ -18,9 +18,9 @@ enum update_status
 typedef unsigned int uint;
 
 // Configuration -----------
-#define SCREEN_SIZE 4
-#define SCREEN_WIDTH 384
-#define SCREEN_HEIGHT 224
+#define SCREEN_SIZE 2.5
+#define SCREEN_WIDTH 228
+#define SCREEN_HEIGHT 278
 #define WIN_FULLSCREEN 0
 #define WIN_FULLSCREEN_DESKTOP 0
 #define WIN_BORDERLESS 0
--- Ms. Pacman/ModuleWindow.cpp
+++ Ms. Pacman/ModuleWindow.cpp
@@ -27,9 +27,9 @@ bool ModuleWindow::Init()
 	else
 	{
 		//Create window
-		int width = SCREEN_WIDTH * 2;
-		int height = SCREEN_HEIGHT * 2;
-		Uint32 flags = SDL_WINDOW_SHOWN;
+		int width = SCREEN_WIDTH * SCREEN_SIZE;
+		int height = SCREEN_HEIGHT * SCREEN_SIZE;
+		Uint32 flags = SDL_WINDOW_RESIZABLE;
 
 		if(WIN_FULLSCREEN == true)
 			flags |= SDL_WINDOW_FULLSCREEN;
